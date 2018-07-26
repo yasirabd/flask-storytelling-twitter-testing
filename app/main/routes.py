d4b7581d1a401058ba3d1a7c67fa3a225f010198
yasirabd <sir.yasirabd@gmail.com>
2018-07-26 22:02:09 +0700

    UI for index

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,15 @@
 from app import db
 from flask import current_app, render_template
 from app.main import bp
+from app.models import Tweet
 
 
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    tweets = Tweet.query.all()
+
+    return render_template('index.html', tweets=tweets)
 
 
 @bp.route('/stories', methods=['GET', 'POST'])
