801549629e9753b8fa62d58401558d6c19abd847
yasirabd <sir.yasirabd@gmail.com>
2018-07-26 21:15:21 +0700

    init db and add table tweet

--- app/__init__.py
+++ app/__init__.py
@@ -60,4 +60,4 @@ def create_app(config_class=Config):
 
     return app
 
-# from app import models
+from app import models
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+
+class Tweet(db.Model):
+    __tablename__ = 'tweet'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(50))
+    username = db.Column(db.String(50))
+    created = db.Column(db.DateTime)
+    text = db.Column(db.String(200))
+    latitude = db.Column(db.Text)
+    longitude = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.username)
