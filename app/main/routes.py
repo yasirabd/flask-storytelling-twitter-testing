f37e35d5187c9362e1169884ceed828a2a06edd6
yasirabd <sir.yasirabd@gmail.com>
2018-07-27 08:56:48 +0700

    observe

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,7 @@
 from app import db
-from flask import current_app, render_template
+from flask import current_app, render_template, request, jsonify
 from app.main import bp
-from app.models import Tweet
+from app.models import Tweet, Test
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -15,3 +15,23 @@ def index():
 @bp.route('/stories', methods=['GET', 'POST'])
 def stories():
     return render_template("stories.html")
+
+
+@bp.route('/process', methods=['GET', 'POST'])
+def process():
+    return render_template("process.html")
+
+
+@bp.route('/process/observe', methods=['GET', 'POST'])
+def test():
+    project_name = request.form['project_name']
+
+    if project_name:
+        tb_test = Test()
+        tb_test.name = project_name
+        db.session.add(tb_test)
+        db.session.commit()
+
+        return jsonify(status_observe="success")
+    else:
+        return jsonify(status_observe="failed")
--- app/models.py
+++ app/models.py
@@ -13,3 +13,12 @@ class Tweet(db.Model):
 
     def __repr__(self):
         return '<Tweet {}>'.format(self.username)
+
+
+class Test(db.Model):
+    __tablename__ = 'test'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<Test {}>'.format(self.name)
